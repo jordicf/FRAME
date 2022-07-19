6da1641c97d5fb718795c956b167c4cae686de21
victor.franco.sanchez <hectobreack123@gmail.com>
2022-07-19 16:57:43 +0200

    Further refactoring of rect

--- tools/rect/main.py
+++ tools/rect/main.py
@@ -50,73 +50,79 @@ def processParam(param: str, i: int, f: float):
     i = i + 1
     return i, f
 
+def enforce_bb(sm, btag, cbtag):
+    """
+    Enforces that a certain rectangle to actually be a rectangle.
+    It also enforces the rectangle to be adjacent to the trunk rectangle,
+    (if the rectangle is not the trunk rectangle already)
+    """
+    var_x = {}
+    var_X = {}
+    var_y = {}
+    var_Y = {}
+    var_b = {}
+    for x in xcoords:
+        var_x[x] = sm.newVar(btag + "x_" + str(x), "")
+        var_X[x] = sm.newVar(btag + "X_" + str(x), "")
+    for y in ycoords:
+        var_y[y] = sm.newVar(btag + "y_" + str(y), "")
+        var_Y[y] = sm.newVar(btag + "Y_" + str(y), "")
+    for b in blocks:
+        var_b[b] = sm.newVar(btag + str(b), "")
+        sm.imply([var_b[b]], var_x[input_problem[b][2]])
+        sm.imply([var_b[b]], var_X[input_problem[b][0]])
+        sm.imply([var_b[b]], var_y[input_problem[b][3]])
+        sm.imply([var_b[b]], var_Y[input_problem[b][1]])
+    for i in range(1, len(xcoords)):
+        sm.imply([var_X[prev_x[xcoords[i]]]], var_X[xcoords[i]])
+        sm.imply([var_x[xcoords[i]]], var_x[prev_x[xcoords[i]]])
+    for i in range(1, len(ycoords)):
+        sm.imply([var_Y[prev_y[ycoords[i]]]], var_Y[ycoords[i]])
+        sm.imply([var_y[ycoords[i]]], var_y[prev_y[ycoords[i]]])
+    for b in blocks:
+        sm.imply([var_x[next_x[input_problem[b][0]]],
+                  var_X[prev_x[input_problem[b][2]]],
+                  var_y[next_y[input_problem[b][1]]],
+                  var_Y[prev_y[input_problem[b][3]]]], var_b[b])
+
+    # Enforce connection to the trunk rectangle
+    if btag != cbtag:
+        north = sm.newVar(btag + "north", "")
+        south = sm.newVar(btag + "south", "")
+        east = sm.newVar(btag + "east", "")
+        west = sm.newVar(btag + "west", "")
+        sm.heuleEncoding([north, south, east, west])
+        sm.pseudoboolEncoding(north + south + east + west >= 1)
+        for b1 in blocks:
+            if input_problem[b1][0] == 0:
+                sm.imply([west], -var_b[b1])
+            if input_problem[b1][1] == 0:
+                sm.imply([north], -var_b[b1])
+            if input_problem[b1][2] == int(ifile['Width']):
+                sm.imply([east], -var_b[b1])
+            if input_problem[b1][3] == int(ifile['Height']):
+                sm.imply([south], -var_b[b1])
+            for b2 in blocks:
+                B1 = input_problem[b1]
+                B2 = input_problem[b2]
+                if B1[0] == B2[2] and B1[3] > B2[1] and B1[1] < B2[3]:
+                    sm.imply([var_b[b1], west, -var_b[b2]], sm.newVar(cbtag + str(b2), ""))
+                if B1[2] == B2[0] and B1[3] > B2[1] and B1[1] < B2[3]:
+                    sm.imply([var_b[b1], east, -var_b[b2]], sm.newVar(cbtag + str(b2), ""))
+                if B1[1] == B2[3] and B1[2] > B2[0] and B1[0] < B2[2]:
+                    sm.imply([var_b[b1], north, -var_b[b2]], sm.newVar(cbtag + str(b2), ""))
+                if B1[3] == B2[1] and B1[2] > B2[0] and B1[0] < B2[2]:
+                    sm.imply([var_b[b1], south, -var_b[b2]], sm.newVar(cbtag + str(b2), ""))
+
 def solve(ratio, dif, nboxes):
     """
     Generates the SAT problem, calls the solver and returns the solution.
-    TODO: Split this function further down into smaller functions: The function is too long.
     :param ratio: The f hyperparameter.
     :param dif: The previous optimal solution, for optimization purposes.
     :param nboxes: The allowed number of boxes of the solution.
     :return: The cost for an existing solution better than the previous one.
     """
     sm = satmanager.SATManager()
-
-    def enforce_bb(btag, cbtag):
-        var_x = {}
-        var_X = {}
-        var_y = {}
-        var_Y = {}
-        var_b = {}
-        for x in xcoords:
-            var_x[x] = sm.newVar(btag + "x_" + str(x), "")
-            var_X[x] = sm.newVar(btag + "X_" + str(x), "")
-        for y in ycoords:
-            var_y[y] = sm.newVar(btag + "y_" + str(y), "")
-            var_Y[y] = sm.newVar(btag + "Y_" + str(y), "")
-        for b in blocks:
-            var_b[b] = sm.newVar(btag + str(b), "")
-            sm.imply([var_b[b]], var_x[input_problem[b][2]])
-            sm.imply([var_b[b]], var_X[input_problem[b][0]])
-            sm.imply([var_b[b]], var_y[input_problem[b][3]])
-            sm.imply([var_b[b]], var_Y[input_problem[b][1]])
-        for i in range(1, len(xcoords)):
-            sm.imply([var_X[prev_x[xcoords[i]]]], var_X[xcoords[i]])
-            sm.imply([var_x[xcoords[i]]], var_x[prev_x[xcoords[i]]])
-        for i in range(1, len(ycoords)):
-            sm.imply([var_Y[prev_y[ycoords[i]]]], var_Y[ycoords[i]])
-            sm.imply([var_y[ycoords[i]]], var_y[prev_y[ycoords[i]]])
-        for b in blocks:
-            sm.imply([var_x[next_x[input_problem[b][0]]],
-                      var_X[prev_x[input_problem[b][2]]],
-                      var_y[next_y[input_problem[b][1]]],
-                      var_Y[prev_y[input_problem[b][3]]]], var_b[b])
-        if btag != cbtag:
-            north = sm.newVar(btag + "north", "")
-            south = sm.newVar(btag + "south", "")
-            east = sm.newVar(btag + "east", "")
-            west = sm.newVar(btag + "west", "")
-            sm.heuleEncoding([north, south, east, west])
-            sm.pseudoboolEncoding(north + south + east + west >= 1)
-            for b1 in blocks:
-                if input_problem[b1][0] == 0:
-                    sm.imply([west], -var_b[b1])
-                if input_problem[b1][1] == 0:
-                    sm.imply([north], -var_b[b1])
-                if input_problem[b1][2] == int(ifile['Width']):
-                    sm.imply([east], -var_b[b1])
-                if input_problem[b1][3] == int(ifile['Height']):
-                    sm.imply([south], -var_b[b1])
-                for b2 in blocks:
-                    B1 = input_problem[b1]
-                    B2 = input_problem[b2]
-                    if B1[0] == B2[2] and B1[3] > B2[1] and B1[1] < B2[3]:
-                        sm.imply([var_b[b1], west, -var_b[b2]], sm.newVar(cbtag + str(b2), ""))
-                    if B1[2] == B2[0] and B1[3] > B2[1] and B1[1] < B2[3]:
-                        sm.imply([var_b[b1], east, -var_b[b2]], sm.newVar(cbtag + str(b2), ""))
-                    if B1[1] == B2[3] and B1[2] > B2[0] and B1[0] < B2[2]:
-                        sm.imply([var_b[b1], north, -var_b[b2]], sm.newVar(cbtag + str(b2), ""))
-                    if B1[3] == B2[1] and B1[2] > B2[0] and B1[0] < B2[2]:
-                        sm.imply([var_b[b1], south, -var_b[b2]], sm.newVar(cbtag + str(b2), ""))
 
     selarea = pseudobool.Expr()
     vars_b = []
@@ -133,6 +139,7 @@ def solve(ratio, dif, nboxes):
     for b in blocks:
         realarea = realarea + area(b, False) * sm.newVar("b_" + str(b), "")
     obj = ratio * selarea - realarea
+    
     # Min area approach
     if ratio < 1:
         sm.pseudoboolEncoding(realarea >= round(theoreticalBestArea * ratio))
@@ -140,8 +147,9 @@ def solve(ratio, dif, nboxes):
     # Min error approach
     else:
         sm.pseudoboolEncoding(obj >= dif[0])
+    
     for i in range(0, nboxes):
-        enforce_bb("b" + str(i) + "_", "b" + str(0) + "_")
+        enforce_bb(sm, "b" + str(i) + "_", "b" + str(0) + "_")
     if not sm.solve():
         print("Insat")
         return (0, 1)
@@ -158,6 +166,7 @@ def solve(ratio, dif, nboxes):
     print("Real area:        " + str(float(sm.evalExpr(realarea)) / float(factor)))
     print("Theoretical area: " + str(theoreticalBestArea / float(factor)))
     print("Error objective:  " + str(sm.evalExpr(obj)))
+    
     # Min area approach
     if ratio < 1:
         return (sm.evalExpr(selarea) + 1, sm.evalExpr(realarea))
@@ -217,12 +226,85 @@ def fstr_to_tuple(p: str, f):
     print(area(inibox, True), area(inibox, False))
     return (round(f * area(inibox, True) - area(inibox, False)), 1)
 
+def defineCoords():
+    """
+    Generates a set of auxiliary variables, useful for many parts of the code.
+    These auxiliary variables are:
+    blocks : A list of the identifiers for the blocks
+    xcoords: An ordered list of all x coordinates
+    ycoords: An ordered list of all y coordinates
+    prev_x : A map from x coordinates to their previous coordinate (if such exists)
+    prev_y : A map from y coordinates to their previous coordinate (if such exists)
+    next_x : A map from x coordinates to their next coordinate (if such exists)
+    next_y : A map from y coordinates to their next coordinate (if such exists)
+    """
+    xset = set()
+    yset = set()
+    blocks = range(0, len(input_problem))
+    
+    for block in input_problem:
+        xset.add(block[0])
+        xset.add(block[2])
+        yset.add(block[1])
+        yset.add(block[3])
+    
+    xcoords = sorted(xset)
+    ycoords = sorted(yset)
+    
+    next_x = {}
+    prev_x = {}
+    next_y = {}
+    prev_y = {}
+    for i in range(1, len(xcoords)):
+        next_x[xcoords[i - 1]] = xcoords[i]
+        prev_x[xcoords[i]] = xcoords[i - 1]
+    for i in range(1, len(ycoords)):
+        next_y[ycoords[i - 1]] = ycoords[i]
+        prev_y[ycoords[i]] = ycoords[i - 1]
+    return blocks, xcoords, ycoords, prev_x, prev_y, next_x, next_y
+
+def selectBox():
+    """
+    Receives the module to optimize and preprocesses the problem to leave
+    only the relevant information for such module.
+    """
+    bmap = [0] * (ifile['Width'] * ifile['Height'])
+    input_problem = []
+    selbox = int(input("Selected module: "))
+    for b in ifile['Rectangles']:
+        for bname in b:
+            [x1, y1, x2, y2, l] = b[bname]
+            input_problem.append((x1, y1, x2, y2, l[selbox]))
+            for i in range(x1, x2):
+                for j in range(y1, y2):
+                    bmap[i + j * ifile['Width']] = len(input_problem) - 1
+    return input_problem, bmap, selbox
+
+def findBestGreedy(f: float):
+    """
+    Calls the greedy algorithm for finding the best solution with just one block.
+    :param f: The f hyperparameter
+    :return: A tuple of the best box, and the quality of the solution
+    """
+    file = open("tofind.txt", "w")
+    file.write(getFile(f))
+    file.close()
+    run = subprocess.Popen(["greedy", "tofind.txt", "bestbox.txt"], stdin=subprocess.PIPE)
+    run.wait()
+    file = open("bestbox.txt", "r")
+    fline = file.readline()
+    [x1, y1, x2, y2, p] = fline.split()
+    inibox = (int(x1), int(y1), int(x2), int(y2), float(p))
+    print(x1, y1, x2, y2, p)
+    return inibox, p
+
 def main():
     """
     Main function.
-    TODO: Split the function further down into smaller functions.
-    This is too long for a main function.
     """
+    global input_problem, factor, ifile, inibox, blocks, xcoords, ycoords
+    global prev_x, prev_y, next_x, next_y, bmap, symbols, selbox
+    global theoreticalBestArea
     
     if len(sys.argv) < 2:
         usage()
@@ -249,55 +331,15 @@ def main():
     with open(file, 'r') as file:
         ifile = yaml.load(file, Loader=yaml.FullLoader)
     
-    bmap = [0] * (ifile['Width'] * ifile['Height'])
-    input_problem = []
-    selbox = int(input("Selected module: "))
-    for b in ifile['Rectangles']:
-        for bname in b:
-            [x1, y1, x2, y2, l] = b[bname]
-            input_problem.append((x1, y1, x2, y2, l[selbox]))
-            for i in range(x1, x2):
-                for j in range(y1, y2):
-                    bmap[i + j * ifile['Width']] = len(input_problem) - 1
+    input_problem, bmap, selbox = selectBox()
     
-    xset = set()
-    yset = set()
-    blocks = range(0, len(input_problem))
-    
-    for block in input_problem:
-        xset.add(block[0])
-        xset.add(block[2])
-        yset.add(block[1])
-        yset.add(block[3])
-    
-    xcoords = sorted(xset)
-    ycoords = sorted(yset)
-    
-    next_x = {}
-    prev_x = {}
-    next_y = {}
-    prev_y = {}
-    for i in range(1, len(xcoords)):
-        next_x[xcoords[i - 1]] = xcoords[i]
-        prev_x[xcoords[i]] = xcoords[i - 1]
-    for i in range(1, len(ycoords)):
-        next_y[ycoords[i - 1]] = ycoords[i]
-        prev_y[ycoords[i]] = ycoords[i - 1]
+    blocks, xcoords, ycoords, prev_x, prev_y, next_x, next_y = defineCoords()
     
     theoreticalBestArea = 0
     for b in blocks:
         theoreticalBestArea += area(b, True)
     
-    file = open("tofind.txt", "w")
-    file.write(getFile(f))
-    file.close()
-    run = subprocess.Popen(["greedy", "tofind.txt", "bestbox.txt"], stdin=subprocess.PIPE)
-    run.wait()
-    file = open("bestbox.txt", "r")
-    fline = file.readline()
-    [x1, y1, x2, y2, p] = fline.split()
-    inibox = (int(x1), int(y1), int(x2), int(y2), float(p))
-    print(x1, y1, x2, y2, p)
+    inibox, p = findBestGreedy(f)
     
     dif = fstr_to_tuple(p, f)  # (0, 1)
     for nboxes in [1, 2, 3]:
