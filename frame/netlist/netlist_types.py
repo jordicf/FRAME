e11cc1b8a497dd207c5138261e772919f18c5d3b
Marçal Comajoan Cara <mcomajoancara@gmail.com>
2022-08-06 17:36:00 +0200

    Refactoring and small fix
    Small bug fix: self.modules -> names in HyperEdge

--- frame/netlist/netlist_types.py
+++ frame/netlist/netlist_types.py
@@ -1,31 +1,21 @@
 from dataclasses import dataclass
+
 from .module import Module
 
 
-# Representation of an edge in a graph (possibly obtained from a hypergraph).
-# The edge represents the target node in an adjacency list
 @dataclass()
 class Edge:
+    """
+    Representation of an edge in a graph (possibly obtained from a hypergraph).
+    The edge represents the target node in an adjacency list
+    """
     node: str  # Name of the target
     weight: float  # Weight of the edge
 
 
-# Representation of a hyperedge (list of modules)
-@dataclass()
-class HyperEdge:
-    modules: list[Module]  # List of modules of the hyperedge
-    weight: float  # Weight of the hyperedge
-
-    def __repr__(self) -> str:
-        names = [b.name for b in self.modules]
-        if self.weight == 1:
-            return f'Edge<modules={names}>'
-        return f'Edge<modules={self.modules}, weight={self.weight}>'
-
-
-# Representation of a hyperedge (list of module names)
 @dataclass()
 class NamedHyperEdge:
+    """Representation of a hyperedge (list of module names)"""
     modules: list[str]  # List of module names of the hyperedge
     weight: float  # Weight of the hyperedge
 
@@ -33,3 +23,16 @@ class NamedHyperEdge:
         if self.weight == 1:
             return f'Edge<modules={self.modules}>'
         return f'Edge<modules={self.modules}, weight={self.weight}>'
+
+
+@dataclass()
+class HyperEdge:
+    """Representation of a hyperedge (list of modules)"""
+    modules: list[Module]  # List of modules of the hyperedge
+    weight: float  # Weight of the hyperedge
+
+    def __repr__(self) -> str:
+        names = [b.name for b in self.modules]
+        if self.weight == 1:
+            return f'Edge<modules={names}>'
+        return f'Edge<modules={names}, weight={self.weight}>'
