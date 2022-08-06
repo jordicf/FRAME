844733878a60c1fe5e043daf57cc3a60ea6cc976
Marçal Comajoan Cara <mcomajoancara@gmail.com>
2022-08-06 18:32:31 +0200

    Add wire_length method for HyperEdge and Netlist and tests
    Added num_edges to Netlist too and additional small tests for Netlist

--- frame/netlist/netlist_types.py
+++ frame/netlist/netlist_types.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
+from math import sqrt
 
 from .module import Module
+from ..geometry.geometry import Point
 
 
 @dataclass()
@@ -30,6 +32,25 @@ class HyperEdge:
     """Representation of a hyperedge (list of modules)"""
     modules: list[Module]  # List of modules of the hyperedge
     weight: float  # Weight of the hyperedge
+
+    @property
+    def wire_length(self) -> float:
+        """
+        Returns the wire length of the hyperedge.
+        The wire length is the distance between the center of each module and the centroid of the
+        modules (without taking into account module areas) multiplied by the hyperedge weight.
+        """
+        intersection_point = Point(0, 0)
+        for b in self.modules:
+            assert b.center is not None, "Module center must be defined. " \
+                                         "Maybe execute b.calculate_center_from_rectangles()?"
+            intersection_point += b.center
+        intersection_point /= len(self.modules)
+        wire_length = 0
+        for b in self.modules:
+            v = intersection_point - b.center
+            wire_length += sqrt(v & v)
+        return wire_length * self.weight
 
     def __repr__(self) -> str:
         names = [b.name for b in self.modules]
